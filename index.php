864d96f1037c65f0d21b853e41bbffc1ef9397be
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-09-16 05:55:07 +0100

    Added temporary landing page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<?php
+    session_start();
+?>
+
+<html>
+
+<head>
+    <!-- Metadata -->
+    <title>Claire Murdoch Counselling</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Claire, Murdoch, Counselling">
+    <meta name="description" content="Counselling portfolio">
+    <meta name="author" content="Chris Murdoch">
+
+    <!-- Styles and scripts -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="centrebox">
+        <div class="ttext">CLAIRE MURDOCH</div>
+        <hr>
+        <div class="stext">Website coming soon.</div>
+    </div>
+    <?php echo "<div style='position: absolute; right: 5px; bottom: 5px; color: green;'>PHP OK!</div>" ?>
+</body>
+
+</html>
